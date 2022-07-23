72c1c9fd18e6b85f995e808711801eb7c135b4d2
unknown <harshmetkel24@gmail.com>
2022-07-23 18:01:22 +0530

    React-Router-Dom version updated to 6

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
-import React from 'react';
-import { Route, Switch } from 'react-router-dom';
-import SI from './Components/SI-units';
-import Error from './Components/error';
-import Conversions from './Components/conversions';
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import SI from "./Components/SI-units";
+import Error from "./Components/error";
+import Conversions from "./Components/conversions";
 
 function App() {
-    return (
-        <main>
-            <Switch>
-                {/* here we have used exact keyword to tell the browser that just look for '/' in url. 
+  return (
+    <main>
+      <Routes>
+        {/* here we have used exact keyword to tell the browser that just look for '/' in url. 
               If don't do this then it will take other things like http://localhost:3000 as the url */}
-                <Route path="/" component={SI} exact />
-                <Route path="/conversions" component={Conversions} />
-                <Route component={Error} />
-            </Switch>
-        </main>
-    );
+        <Route path="/" element={<SI />} />
+        <Route path="/conversions" element={<Conversions />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
+    </main>
+  );
 }
 
 export default App;
